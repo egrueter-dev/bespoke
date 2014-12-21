2f5e5acb3d811e95bf4da2bc2289421d7512e610
Erik <erikgrueter@gmail.com>
2014-12-21 20:51:03 +0100

    fixed it, server works fine, though not sure why removing ~/ worked..

--- webrick.rb
+++ webrick.rb
@@ -1,5 +1,5 @@
 require 'webrick'
-root = File.expand_path '~/index.html'
+root = File.expand_path '' #not sure why that worked, and ~/ didnt.....
 server = WEBrick::HTTPServer.new :Port => 4000, :DocumentRoot => root
  trap 'INT' do server.shutdown end
  server.start
